f76d51d58bacb4fef389ce71756d628266e71c92
ANkulagin <ankulskull@gmail.com>
2024-02-08 17:23:37 +0300

    Separate client and kernel code

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,8 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\TrainerController;
-use App\Router\Route;
+use App\Kernel\Router\Route;
+
 return [
     Route::get('/home',[HomeController::class,'index']),
     Route::get('/trainer',[TrainerController::class,'index']),
